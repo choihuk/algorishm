ab1de9f0e0c76b6e92db0fad90493f4a58c700f2
choihuk <love72huk@naver.com>
2022-02-25 05:26:27 +0000

    .

--- beakjoon/Greedy/BJ1541.java
+++ beakjoon/Greedy/BJ1541.java
@@ -4,6 +4,9 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class BJ1541{
+    // 문자열을 숫자와 연산자로 나누는 과정이 힘들었음. (정규화를 몰라..)
+    // 근데 굳이 나누지 않고 for(char c : input.toCharArray()) 로 c=='+'나 c=='-'
+    // 에 따라 여태껏 쌓아둔 숫자를 더하거나 빼는 작업으로 문제를 풀어도 됐음...
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
@@ -13,6 +16,7 @@ public class BJ1541{
         Queue<String> operator = new LinkedList<>();
         for(String n:num) numbers.add(Integer.parseInt(n));
         for(String c : cal) if(!c.equals("")) operator.add(c);
+        
         int result = numbers.poll();
         while(operator.peek()!=null){
             String o = operator.poll();

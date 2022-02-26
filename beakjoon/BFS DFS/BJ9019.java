c842706bcc36bbab9d45e71d99052f9f60d9fc0c
choihuk <love72huk@naver.com>
2022-02-26 05:26:56 +0000

    .

--- beakjoon/BFS DFS/BJ9019.java
+++ beakjoon/BFS DFS/BJ9019.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 // Integer.parseInt나 Integer.toString을 남발하면 계산속도가 기하급수적으로 커진다는 것을 몸소 체험한 문제... String형 말고 int형으로 사용해도 풀 수 있는 문제여따..
+// 
 public class BJ9019{
     private static int B;
     private static HashSet<Integer> set;
@@ -56,4 +57,35 @@ public class BJ9019{
     private static int R(int a){
         return a%10*1000 + a/10;
     }
+    // 참고 코드: 결과값을 command에 넣어서 command의 유무로 memoization을 했다.
+    // 순회하면서 결과들을 넣는 방식 너무 좋은데..? 만약 결과값이 있음 중복해서 안돌아도 되고.
+    // static String bfs(int a, int b) {
+    //     Queue<Integer> queue = new LinkedList<>();
+    //     queue.add(a);
+    //     String command[] = new String[10000];
+    //     command[a] = "";
+    //     int dslr, d, s, l, r;
+    //     while(!queue.isEmpty() && command[b] == null) {
+    //         dslr = queue.remove();
+    //         d = (dslr * 2) % 10000;
+    //         if(command[d] == null) {
+    //             command[d] = command[dslr] + "D";
+    //             queue.add(d);
+    //         }
+    //         s = dslr == 0 ? 9999 : dslr-1;
+    //         if(command[s] == null) {
+    //             command[s] = command[dslr] + "S";
+    //             queue.add(s);
+    //         }
+    //         l = dslr % 1000 * 10 + dslr / 1000;
+    //         if(command[l] == null) {
+    //             command[l] = command[dslr] + "L";
+    //             queue.add(l);
+    //         }
+    //         r = dslr % 10 * 1000 + dslr / 10;
+    //         if(command[r] == null) {
+    //             command[r] = command[dslr] + "R";
+    //             queue.add(r);
+    //         }
+    //     }
 }

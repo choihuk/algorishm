14a8ebf9cc4de6b6e3bf334269974a5717a969b9
choihuk <love72huk@naver.com>
2022-03-09 07:19:59 +0000

    .

--- programmers/kakao_2019_winter_internship/five.java
+++ programmers/kakao_2019_winter_internship/five.java
@@ -1,23 +1,20 @@
-public class five{
-    public static void main(String[] args){
-        int[] stones = {2, 4, 5, 3, 2, 1, 4, 2, 5, 1};
-        System.out.println(solution(stones,3));
-    }
-    public static int solution(int[] stones, int k) {
-        int right = 5;
+class Solution {
+    public int solution(int[] stones, int k) {
+        int right = 10;
+        for(int stone : stones) if(right<stone) right=stone;
         int left = 1;
         while(right>left){
             int mid = (right+left)/2;
             int cnt = 1;
             int max = 0;
             for(int stone : stones){
-                if(stone-mid>=0) cnt = 1;
-                else cnt+=1;
+                if(stone-mid<0) cnt += 1;
+                else cnt = 1;
                 if(cnt>max) max = cnt;
             }
-            if(max<k) left = mid;
-            else right = mid+1;
+            if(max<=k) left = mid+1;
+            else right = mid;
         }
-        return right;
+        return right-1;
     }
 }

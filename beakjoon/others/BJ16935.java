5569e3cb46e598ce406b7280079b1379300e846e
choihuk <love72huk@naver.com>
2022-07-16 00:17:12 +0900

    commit

--- beakjoon/others/BJ16935.java
+++ beakjoon/others/BJ16935.java
@@ -8,13 +8,14 @@ import java.util.StringTokenizer;
 public class BJ16935 {
     private static int N;
     private static int M;
+    private static int[][] matrix;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         int R = Integer.parseInt(st.nextToken());
-        int[][] matrix = new int[N][M];
+        matrix = new int[N][M];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
@@ -25,50 +26,95 @@ public class BJ16935 {
         st = new StringTokenizer(br.readLine());
         while(R --> 0) queue.add(Integer.parseInt(st.nextToken()));
 
-        switch (queue.poll()) {
-            case 1 : cal1(matrix); break;
-            case 2 : cal2(matrix); break;
-            case 3 : cal3(matrix); break;
-            case 4 : cal4(matrix); break;
-            case 5 : cal5(matrix); break;
-            case 6 : cal6(matrix); break;
+        while (!queue.isEmpty()) {
+            switch (queue.poll()) {
+                case 1 : cal1(); break;
+                case 2 : cal2(); break;
+                case 3 : cal3(); break;
+                case 4 : cal4(); break;
+                case 5 : cal5(); break;
+                case 6 : cal6(); break;
+            }
         }
-    }
-
-    private static void cal6(int[][] matrix) {
-
-    }
-
-    private static void cal5(int[][] matrix) {
-
-    }
-
-    private static void cal4(int[][] matrix) {
-
-    }
-
-    private static void cal3(int[][] matrix) {
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < M / 2; i++) {
-//            int x =
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                sb.append(matrix[i][j]).append(" ");
+            }
+            sb.append("\n");
         }
+        System.out.println(sb.toString());
     }
 
-    private static void cal2(int[][] matrix) {
+    private static void cal6() {
+        int[][] n = new int[N][M];
+        for (int i = 0; i < N/2; i++) {
+            for (int j = 0; j < M/2; j++) {
+                n[i][j] = matrix[i][M / 2 + j];
+                n[i][M / 2 + j] = matrix[N / 2 + i][M / 2 + j];
+                n[N / 2 + i][M / 2 + j] = matrix[N / 2 + i][j];
+                n[N / 2 + i][j] = matrix[i][j];
+            }
+        }
+        matrix = n;
+    }
+
+    private static void cal5() {
+        int[][] n = new int[N][M];
+        for (int i = 0; i < N/2; i++) {
+            for (int j = 0; j < M/2; j++) {
+                n[i][j] = matrix[N / 2 + i][j];
+                n[i][M / 2 + j] = matrix[i][j];
+                n[N / 2 + i][M / 2 + j] = matrix[i][M / 2 + j];
+                n[N / 2 + i][j] = matrix[N / 2 + i][M / 2 + j];
+            }
+        }
+        matrix = n;
+    }
+
+    private static void cal4() {
+        int[][] n = new int[M][N];
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                n[i][j] = matrix[j][M-i-1];
+            }
+        }
+        int temp = M;
+        M = N;
+        N = temp;
+        matrix = n;
+    }
+
+    private static void cal3() {
+        int[][] n = new int[M][N];
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                n[i][j] = matrix[N-j-1][i];
+            }
+        }
+        int temp = M;
+        M = N;
+        N = temp;
+        matrix = n;
+    }
+
+    private static void cal2() {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M / 2; j++) {
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][M - j];
-                matrix[i][M - j] = temp;
+                matrix[i][j] = matrix[i][M - j - 1];
+                matrix[i][M - j - 1] = temp;
             }
         }
     }
 
-    private static void cal1(int[][] matrix) {
+    private static void cal1() {
         for (int i = 0; i < N / 2; i++) {
-            int[] temp = matrix[i];
-            matrix[i] = matrix[N - i];
-            matrix[N - 1] = temp;
+            for (int j = 0; j < M; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[N - i - 1][j];
+                matrix[N - i - 1][j] = temp;
+            }
         }
     }
 
---
+++ beakjoon/unsolved/test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args) {
+        int[] arr = new int[1];
+        System.out.println(arr);
+        System.out.println(a(arr));
+    }
+
+    private static int[] a(int[] arr) {
+        System.out.println(arr);
+        return arr;
+    }
+}
